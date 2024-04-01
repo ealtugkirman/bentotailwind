d21fa7056b73d922176a188ff666ae207cb5adbc
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-04-01 20:42:18 +0300

    googleid

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,11 +2,12 @@ import { Georama, Josefin_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Layout/Navbar";
 import Duyuru from "../components/ui/Duyuru";
-
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const darker = Georama({ subsets: ["latin"] });
 const jos = Josefin_Sans({ subsets: ["latin"] });
 
+const GOOGLEID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
 
 export const metadata = {
   title: "BentoTailwind.com",
@@ -16,10 +17,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={ jos.className }>
-        <Duyuru/>
+      <GoogleTagManager gtmId={GOOGLEID} />
+      <body className={jos.className}>
+        <Duyuru />
         <Navbar />
-        { children }
+        {children}
       </body>
     </html>
   );
