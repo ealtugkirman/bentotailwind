346622b5a47fd5aac16118ba59c8d969750f3bd1
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-05-20 13:10:11 +0300

    hey

--- src/app/components/_components/SideBar.jsx
+++ src/app/components/_components/SideBar.jsx
@@ -58,13 +58,13 @@ const SectionsList = ({ items }) => (
 const Sidebar = () => {
     const lessons = {
         basicGrids: [{ name: "Simple Bento Grid", href: "/components/simple-bento" }, { name: "Simple Bento With Text", href: "/components/simple-bento-with-text" }, { name: "Tailwind Responsive Grid", href: "/components/tailwind-responsive-grid" },],
-        bentoGrids: [{ name: "Reveal Bento", href: "/components/reveal-bento" }, { name: "Fade In Grid", href: "/components/fadein-bento-grid" }, { name: "Coming Soon...", href: "/components/coming-soon" }]
+        bentoGrids: [{ name: "Reveal Bento", href: "/components/reveal-bento" }, { name: "Fade In Grid", href: "/components/fadein-bento-grid" }, { name: "Github Copilot Grid", href: "/components/github-copilot-bento" }, { name: "Coming Soon...", href: "/components/coming-soon" }]
     }
 
     return (
         <>
             <nav
-                className="fixed hidden md:flex z-10 pb-20 bottom-20 top-32 left-0 w-full h-full border-r bg-first space-y-8 overflow-auto sw-64">
+                className="fixed hidden md:flex z-10 pb-20 bottom-20 top-32 left-0  h-full border-r bg-first space-y-8 overflow-auto sw-64">
                 <div className='text-[0.9rem] space-y-6'>
                     <>
                         <div>
