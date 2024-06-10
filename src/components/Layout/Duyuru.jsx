bfd908855218755f508f12f182e4076ac2ee3125
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-06-10 23:57:20 +0300

    81

--- src/components/Layout/Duyuru.jsx
+++ src/components/Layout/Duyuru.jsx
@@ -2,22 +2,7 @@
 import { useState, useEffect } from "react";
 
 const Duyuru = () => {
-  const [userCount, setUserCount] = useState(null);
-
-  const fetchUserCount = async () => {
-    try {
-      const response = await fetch("/api/userCount");
-      const data = await response.json();
-      setUserCount(data.userCount);
-    } catch (error) {
-      console.error("Kullanıcı sayısı çekilemedi:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchUserCount();
-  }, []);
-
+ 
   return (
     <div className="bg-yellow-200">
       <div className="max-w-screen-xl mx-auto px-4 py-3 text-firstdark sm:text-center md:px-8">
@@ -26,11 +11,7 @@ const Duyuru = () => {
           <strong>free for the first 100 users. </strong>
           <br />
           <div>
-            {userCount !== null ? (
-              <span>{` Remaining Seat: ${95 - userCount}`}</span>
-            ) : (
-              <span>Checking for empty seats...</span>
-            )}
+              <span>Remaining Seat: 81</span>
           </div>
           <button className="px-3 ml-4 py-1 font-medium bg-yellow-400 text-firstdark w-fit transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
             <a href="https://accounts.bentotailwind.com/sign-up">Sign Up Now</a>
