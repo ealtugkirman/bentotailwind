2bd458d31802695103e6ad8f5b6424a8c84a6f63
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-09-26 08:47:04 +0300

    countdown completed

--- src/components/Layout/Duyuru.jsx
+++ src/components/Layout/Duyuru.jsx
@@ -4,12 +4,9 @@ const Duyuru = () => {
     <div className="bg-yellow-200">
       <div className="max-w-screen-xl mx-auto px-4 py-3 text-firstdark sm:text-center md:px-8">
         <div className="text-sm">
-          Limited Offer: BentoTailwind is{" "}
-          <strong>free for the first 100 users. </strong>
-          <br />
-          <div className="capitalize" >
-              <span>Remaining Seat: 12</span>
-          </div>
+          BentoTailwind has reached
+          <strong>100 users. 🥳🥳🥳</strong>
+         
           <button className="px-3 ml-4 py-1 font-medium bg-yellow-400 text-firstdark w-fit transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
             <a href="https://accounts.bentotailwind.com/sign-up">Sign Up Now</a>
           </button>
